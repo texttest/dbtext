64b0553bd75b536d7be4b2cc53c9f7437446d44c
Geoff Bache <geoff.bache@evry.com>
2020-06-07 10:30:04 +0200

    setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='dbtext',
+      version='0.1',
+      description='Database to text utility for approval testing',
+      author='Johan Andersson,Geoff Bache',
+      author_email='geoff.bache@pobox.com',
+      url='https://github.com/texttest/dbtext',
+      py_modules=['dbtext'],
+      )
