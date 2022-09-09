9388c16a681f045059b65f097fc4f0c3625a11b4
Emily Bache <emily@bacheconsulting.com>
2022-09-09 12:02:13 +0000

    useful utility for dumping pandas dataframe as json

--- dbtext/db_to_text.py
+++ dbtext/db_to_text.py
@@ -1,6 +1,14 @@
 
+import os
 
-def dump_dataframe(df, tablename, ext="table", prefix=""):
+
+def dump_dataframe_as_json(df, tablename, folder="db_tables", prefix=""):
+    """Dump a pandas dataframe in json format suitable to use with dbtext"""
+    path = os.path.join(folder, f"{prefix}{tablename}.json")
+    df.to_json(path, orient="records", indent=2)
+
+
+def dump_dataframe_as_rowdata(df, tablename, ext="table", prefix=""):
     """Dump a pandas dataframe in dbtext format"""
     with open(f"{prefix}{tablename}.{ext}", "w") as f:
         for i, tup in enumerate(df.itertuples(index=False)):
